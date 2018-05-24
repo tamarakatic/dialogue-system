b5a166c9cd24dc6d28a18ff747b665e9b3b01264
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2018-05-24 17:40:04 +0200

    Record audio in the browser

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,41 @@
+class Recorder {
+  constructor() {
+    this.audioBinary = null;
+  }
+
+  record(recordingTime) {
+    navigator
+      .mediaDevices
+      .getUserMedia({ audio: true })
+      .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        const audioChunks = [];
+        mediaRecorder.addEventListener("dataavailable", event => {
+          audioChunks.push(event.data);
+        });
+
+        mediaRecorder.addEventListener("stop", () => {
+          this.audioBinary = new Blob(audioChunks);
+        });
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+          this.sendAudioToServer();
+        }, recordingTime);
+      });
+  }
+
+  sendAudioToServer() {
+    // TODO
+    // Post audio binary to server app
+  }
+}
+
 $(() => {
-  $('#record-btn').click(() => alert('Recording ...'));
+  const recordingTime = 3 * 1000; // 3s
+  const recorder = new Recorder();
+
+  $('#record-btn').click(() => recorder.record(recordingTime));
 });
