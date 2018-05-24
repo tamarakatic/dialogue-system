491ab1c6a5e8f49c1f91f4d44e2f56f05f1fd919
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2018-05-24 22:20:09 +0200

    Add development env configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
+import os
+
 from flask import Flask
 
+config = {
+    'development': 'app.config.Development',
+    'production': 'app.config.Production',
+    'default': 'app.config.Development',
+}
+
+env = os.getenv('FLASK_ENV', 'default')
+
 app = Flask(__name__)
-app.config.from_object('app.config')
+app.config.from_object(config[env])
 
 from app import routes  # noqa
